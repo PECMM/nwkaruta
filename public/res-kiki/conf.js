f8820ab29cb027d0cd8ddc86f45a61ca4ad7a196
PECMM <pecmmhnst@gmail.com>
2022-04-06 00:33:36 +0900

    キキ様かるた リソース追加
    キキ様の読み上げボイスおよび画像3点追加

--- public/res-kiki/conf.js
+++ public/res-kiki/conf.js
@@ -25,12 +25,15 @@ const settings = {
   
   cards: [
     { id:'a3', label:'う', imgPath:'a3.jpg', soundPath:'a3.mp3', },
+    { id:'a5', label:'お', imgPath:'a5.jpg', soundPath:'a5.mp3', },
     { id:'k1', label:'か', imgPath:'k1.jpg', soundPath:'k1.mp3', },
     { id:'k4', label:'け', imgPath:'k4.jpg', soundPath:'k4.mp3', },
     { id:'s3', label:'す', imgPath:'s3.jpg', soundPath:'s3.mp3', },
     { id:'s5', label:'そ', imgPath:'s5.jpg', soundPath:'s5.mp3', },
+    { id:'t1', label:'た', imgPath:'t1.jpg', soundPath:'t1.mp3', },
     { id:'n2', label:'に', imgPath:'n2.jpg', soundPath:'n2.mp3', },
     { id:'h1', label:'は', imgPath:'h1.jpg', soundPath:'h1.mp3', },
+    { id:'h5', label:'ほ', imgPath:'h5.jpg', soundPath:'h5.mp3', },
     { id:'m4', label:'め', imgPath:'m4.jpg', soundPath:'m4.mp3', },
     { id:'y1', label:'や', imgPath:'y1.jpg', soundPath:'y1.mp3', },
   ],
