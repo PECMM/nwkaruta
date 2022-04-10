7922c8c5374786b93d6b9a4d3c397ef9d0439db9
PECMM <pecmmhnst@gmail.com>
2022-04-10 19:39:56 +0900

    かるた バグ修正 v0.3.1 およびリソース追加
    * localStorage のキー指定が誤っていたバグを修正
    * キキ様の読み上げボイスおよび画像2点追加

--- public/res-kiki/conf.js
+++ public/res-kiki/conf.js
@@ -27,6 +27,7 @@ const settings = {
     { id:'a3', label:'う', imgPath:'a3.jpg', soundPath:'a3.mp3', },
     { id:'a5', label:'お', imgPath:'a5.jpg', soundPath:'a5.mp3', },
     { id:'k1', label:'か', imgPath:'k1.jpg', soundPath:'k1.mp3', },
+    { id:'k2', label:'き', imgPath:'k2.jpg', soundPath:'k2.mp3', },
     { id:'k4', label:'け', imgPath:'k4.jpg', soundPath:'k4.mp3', },
     { id:'s3', label:'す', imgPath:'s3.jpg', soundPath:'s3.mp3', },
     { id:'s5', label:'そ', imgPath:'s5.jpg', soundPath:'s5.mp3', },
@@ -37,6 +38,7 @@ const settings = {
     { id:'m4', label:'め', imgPath:'m4.jpg', soundPath:'m4.mp3', },
     { id:'y1', label:'や', imgPath:'y1.jpg', soundPath:'y1.mp3', },
     { id:'r1', label:'ら', imgPath:'r1.jpg', soundPath:'r1.mp3', },
+    { id:'w5', label:'を', imgPath:'w5.jpg', soundPath:'w5.mp3', },
   ],
 };
 
